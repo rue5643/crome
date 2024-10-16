7e41d7d8e98605608b0ebe39f33452dd8b448358
rue5643 <137784943+rue5643@users.noreply.github.com>
2024-10-16 16:18:46 +0900

    Update bookmark-toggle.js

--- bookmark-toggle.js
+++ bookmark-toggle.js
@@ -16,3 +16,20 @@ if(isBookMarkBarOpen==="close"){
 // 북마크 toggle
 const bookmarkBarToggle=()=>{
   let isBookMarkBarOpen=localStorage.getItem("isBookmarkBarOpen");
+
+  if(isBookMarkBarOpen==="close"){
+    localStorage.setItem("isBookMarkBarOpen","open");
+    bookmarkBar.style.display="block";
+    bookMarkOpen.style.display="flex";
+    bookMarkClose.style.display="none";
+    return;
+  }
+
+  locarStorage.setItem("isBookMarkBarOpen","close");
+  bookmarkBar.style.display="none";
+  bookMarkOpen.style.display="none";
+  bookMarkClose.style.display="flex";
+};
+
+document.getElementById("bookmark-open-btn").addEventListener("click",bookmarkBarToggle);
+document.getElementById("bookmark-close-btn").addEventListener("click", bookmarkBarToggle);
