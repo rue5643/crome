ad15307d82a1715122f22146be8d5ee9029155da
rue5643 <137784943+rue5643@users.noreply.github.com>
2024-10-17 08:38:15 +0900

    Update bookmark-toggle.js

--- bookmark-toggle.js
+++ bookmark-toggle.js
@@ -1,35 +1,29 @@
-const bookmakrBar=document.getElementById("bookmark-bar");
-const bookMarkOpen=document.getElementById("bookmark-open");
-const bookMarkClose=document.getElementById("bookmark-close");
+const bookmarkBar = document.getElementById("bookmark-bar");
+const bookMarkOpen = document.getElementById("bookmark-open");
+const bookMarkClose = document.getElementById("bookmark-close");
 
-// 북마크
-const isBookMarkBarOpen=localStorage.getItem("isBookMarkBarOpen");
-if(isBookMarkBarOpen==="close"){
-  bookmarkBar.style.display="none";
-  bookMarkOpen.style.display="none";
-  bookMarkClose.style.display="flex";
-}else{
-  bookmarkBar.style.display="block";
-  bookMarkOpen.style.display="flex";
-  bookMarkClose.style.display="none";
-}
-// 북마크 toggle
-const bookmarkBarToggle=()=>{
-  let isBookMarkBarOpen=localStorage.getItem("isBookmarkBarOpen");
-
-  if(isBookMarkBarOpen==="close"){
-    localStorage.setItem("isBookMarkBarOpen","open");
-    bookmarkBar.style.display="block";
-    bookMarkOpen.style.display="flex";
-    bookMarkClose.style.display="none";
-    return;
-  }
-
-  locarStorage.setItem("isBookMarkBarOpen","close");
-  bookmarkBar.style.display="none";
-  bookMarkOpen.style.display="none";
-  bookMarkClose.style.display="flex";
+// <--북마크 바 toggle-->
+const bookmarkBarToggle = () => {
+    let isBookMarkBarOpen = localStorage.getItem("isBookMarkBarOpen");
+    if (isBookMarkBarOpen) {
+        if (isBookMarkBarOpen === "open") {
+            localStorage.setItem("isBookMarkBarOpen", "close");
+						bookmarkBar.style.display = "none";
+				    bookMarkOpen.style.display = "none";
+				    bookMarkClose.style.display = "flex";
+        } else {
+            localStorage.setItem("isBookMarkBarOpen", "open");
+            bookmarkBar.style.display = "block";
+            bookMarkOpen.style.display = "flex";
+            bookMarkClose.style.display = "none";
+        }
+    } else {
+        localStorage.setItem("isBookMarkBarOpen", "close");
+        bookmarkBar.style.display = "none";
+		    bookMarkOpen.style.display = "none";
+		    bookMarkClose.style.display = "flex";
+    }
 };
 
-document.getElementById("bookmark-open-btn").addEventListener("click",bookmarkBarToggle);
+document.getElementById("bookmark-open-btn").addEventListener("click", bookmarkBarToggle);
 document.getElementById("bookmark-close-btn").addEventListener("click", bookmarkBarToggle);
